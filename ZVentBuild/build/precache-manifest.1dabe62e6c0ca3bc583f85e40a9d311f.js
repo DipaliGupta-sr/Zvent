620c2254c31d7445b18f98f21927afb82d1364ad
dipali1357 <dipaligupta45j@gmail.com>
2020-06-02 18:17:22 +0530

    InitialDemo

---
+++ ZVentBuild/build/precache-manifest.1dabe62e6c0ca3bc583f85e40a9d311f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf823df3f1af64ccd8220f34d52f27f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c913787f48c78b05bff4",
+    "url": "/static/css/main.c0725a64.chunk.css"
+  },
+  {
+    "revision": "cace6d4a4347d4b276ea",
+    "url": "/static/js/2.cacea99d.chunk.js"
+  },
+  {
+    "revision": "f36b17b811c9eb7196f45d544d30b66d",
+    "url": "/static/js/2.cacea99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c913787f48c78b05bff4",
+    "url": "/static/js/main.c33c721e.chunk.js"
+  },
+  {
+    "revision": "e66d0bcc170f49200371",
+    "url": "/static/js/runtime-main.03cbd11e.js"
+  }
+]);
